77339b027adb650f88310352fdcba453bfb5d5bb
njse22 <njse22@gmail.com>
2026-04-16 09:10:29 -0500

    04_thread_pool

--- 04_thread_pool/app/src/main/java/com/example/App.java
+++ 04_thread_pool/app/src/main/java/com/example/App.java
@@ -3,6 +3,7 @@ package com.example;
 import java.net.ServerSocket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 
 // Servidor
@@ -14,14 +15,21 @@ public class App {
 
     public static void main(String[] args) {
         ExecutorService pool = Executors.newFixedThreadPool(3); 
+
+        // Semaphore 
+        Semaphore semaphore = new Semaphore(3);
         // Try with resources 
         try (ServerSocket socket = new ServerSocket(5000)) {
 
+            semaphore.acquire();
             pool.execute(new ClientHandler(socket.accept()));
             
         } catch (Exception e) {
             // TODO: handle exception
             pool.shutdown();
+        }
+        finally{
+            semaphore.release();
         }
     }
 }

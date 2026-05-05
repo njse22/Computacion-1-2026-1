e9dadb5dd11dc1e2110897e8032549ffeb1a89d4
njse22 <njse22@gmail.com>
2026-05-05 10:25:04 -0500

    06_dom

---
+++ 06_dom/index.js
@@ -0,0 +1,14 @@
+// carga los elementos definidos dentro de esta 
+// función antes de renderizar la página 
+//
+window.onload  = () => {
+
+
+    sum()
+}
+
+function sum(){
+    alert(10 + 30)
+
+}
+
